e2b890083e2bc9981570009d0d3802ede7f0b302
yurasulima <yurasulima124@gmail.com>
2024-07-09 15:35:02 +0300

    init

---
+++ src/main/java/org/example/dao/ClientDao.java
@@ -0,0 +1,43 @@
+package org.example.dao;
+
+import org.example.HibernateUtil;
+import org.example.dao.model.Client;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class ClientDao {
+
+    private final SessionFactory sessionFactory = HibernateUtil.getInstance().getSessionFactory();
+
+    public void save(Client client) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.persist(client);
+            tx.commit();
+        }
+    }
+
+    public Client findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Client.class, id);
+        }
+    }
+
+    public void update(Client client) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.merge(client);
+            tx.commit();
+        }
+    }
+
+    public void delete(Client client) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.remove(client);
+            tx.commit();
+        }
+    }
+
+}
---
+++ src/main/java/org/example/dao/PlanetDao.java
@@ -0,0 +1,41 @@
+package org.example.dao;
+
+import org.example.HibernateUtil;
+import org.example.dao.model.Planet;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class PlanetDao {
+    private final SessionFactory sessionFactory = HibernateUtil.getInstance().getSessionFactory();
+
+    public void save(Planet planet) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.persist(planet);
+            tx.commit();
+        }
+    }
+
+    public Planet findById(String id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Planet.class, id);
+        }
+    }
+
+    public void update(Planet planet) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.merge(planet);
+            tx.commit();
+        }
+    }
+
+    public void delete(Planet planet) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.remove(planet);
+            tx.commit();
+        }
+    }
+}
